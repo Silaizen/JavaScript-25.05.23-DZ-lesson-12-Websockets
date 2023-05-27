ff516f3b2866c709181895ca3207e182fa86bdbb
Silaizen01~ <hidclift@gmail.com>
2023-05-28 00:30:53 +0300

    converting a string to a number

--- 9_task/1.js
+++ 9_task/1.js
@@ -1,20 +1,25 @@
 let score = 0;
 
-
-let answer1 = parseInt( prompt("Яка найбільша планета Сонячної системи?\n1. Марс\n2. Юпітер\n3. Венера"));
-if (answer1 === "2") {
+let answer1 = parseInt(
+  prompt(
+    "Яка найбільша планета Сонячної системи?\n1. Марс\n2. Юпітер\n3. Венера"
+  )
+);
+if (answer1 === 2) {
   score += 2;
 }
 
-
-let answer2 = parseInt( prompt("Яка найбільша країна за площею?\n1. Канада\n2. Росія\n3. Китай"));
-if (answer2 === "2") {
+let answer2 = parseInt(
+  prompt("Яка найбільша країна за площею?\n1. Канада\n2. Росія\n3. Китай")
+);
+if (answer2 === 2) {
   score += 2;
 }
 
-
-let answer3 = parseInt( prompt("Як називається столиця Японії?\n1. Токіо\n2. Кіото\n3. Осака"));
-if (answer3 === "1") {
+let answer3 = parseInt(
+  prompt("Як називається столиця Японії?\n1. Токіо\n2. Кіото\n3. Осака")
+);
+if (answer3 === 1) {
   score += 2;
 }
 
